e4da250229c1c458b7780ab0d195776370442b67
xiaofei1992 <shophy.cheng@gmail.com>
2013-12-12 16:33:58 +0800

    creating home, about and contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Pinteresting::Application.routes.draw do
+   get "pages/home"
+   get "pages/about"
+      get "pages/contact"
+   # root "pages#home"
+   	get "home"  => "pages#home"
+    get "about" => "pages#about"
+    get "contact" => "pages#contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
