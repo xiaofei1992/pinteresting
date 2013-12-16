1bd169fdc4e39c63df7a9e0352525cb5b007ec64
xiaofei1992 <shophy.cheng@gmail.com>
2013-12-16 21:35:56 +0800

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Pinteresting::Application.routes.draw do
     get 'about' => 'pages#about'
     get 'contact' => 'pages#contact'
 
-	root :to => 'pages#home' 
+	# root :to => 'pages#home' 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
